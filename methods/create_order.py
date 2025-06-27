4854a968d5779c7d8092158572204113d0b06340
Andrei Borodin <andrei.borodin@dcsportsoft.com>
2025-06-27 18:27:52 +0300

    Add test_get_orders.py

--- methods/create_order.py
+++ methods/create_order.py
@@ -1,7 +1,7 @@
 from json import JSONDecodeError
 
 import requests
-from data.endpoints import BASE_URL, GET_ORDER_URL
+from data.endpoints import BASE_URL, ORDER_URL
 
 
 class CreateOrder:
@@ -9,7 +9,7 @@ class CreateOrder:
     @staticmethod
     def post_create_order(ingredients=None, token=None):
         headers = {"Authorization": token}
-        response = requests.post(f"{BASE_URL}{GET_ORDER_URL}", json=ingredients, headers=headers)
+        response = requests.post(f"{BASE_URL}{ORDER_URL}", json=ingredients, headers=headers)
         try:
             return response.status_code, response.json()
         except JSONDecodeError:
